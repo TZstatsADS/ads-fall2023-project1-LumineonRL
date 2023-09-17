b3a6d3c60f439de3356088f6d2d39c63295fbdd9
LumineonRL <thebaconator25@aim.com>
2023-09-17 15:02:58 -0400

    Add script to load data

---
+++ lib/load_data.R
@@ -0,0 +1,29 @@
+library(readr)
+library(stringr)
+library(magrittr)
+library(here)
+library(dplyr)
+
+load_data <- function() {
+  data_subfolder <- "data/happydb/data"
+  topic_subfolder <- "data/happydb/data/topic_dict"
+  
+  files_data <- list.files(here(data_subfolder), ) %>%
+    str_subset("\\.csv$") %>%
+    as_tibble() %>%
+    mutate(path = str_c(data_subfolder, "/", value))
+  
+  files_topic_dict <- list.files(here(topic_subfolder), ) %>%
+    str_subset("\\.csv$") %>%
+    as_tibble() %>%
+    mutate(path = str_c(topic_subfolder, "/", value))
+  
+  files_df <- files_data %>%
+    rbind(files_topic_dict) %>%
+    mutate(df_name = str_split_i(value, "\\.", 1))
+  
+  for(files in 1L:nrow(files_df)) {
+    assign(files_df$df_name[files], read_csv(here(files_df$path[files])))
+  }
+}
+
